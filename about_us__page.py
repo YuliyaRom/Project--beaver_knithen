692d6fd3a13ac6ae9d5b1c67af05b07dfa713ddc
Aleksandr Kamyshenkov <straidem@gmail.com>
2022-10-04 20:11:15 +0300

    Alex flask chapter about us

--- about_us__page.py
+++ about_us__page.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def about_us():
     page_title = 'Beaver_knithen: О нас'
     beaver_knithen_text = "Компания Beaver_knithen представляет"
-    return render_template('index.html', beaver_text=beaver_knithen_text, title=page_title)
+    return render_template('about_us.html', beaver_text=beaver_knithen_text, title=page_title)
 
 
 
